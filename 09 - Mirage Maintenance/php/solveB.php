c59dfee7408f3ecc9a3a5f5cd4581147528debd8
mhepburn <mhepburn@podfather.com>
2023-12-09 20:39:08 +0000

    Solve 09 B

---
+++ 09 - Mirage Maintenance/php/solveB.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace AoC\Nine;
+
+use AoC\Common\Logger;
+
+require_once __DIR__ . '/common.php';
+require_once __DIR__ . '/../../common/php/Logger.php';
+
+$logger = new Logger();
+
+$sequences = getSequences();
+
+$extrapolated = [];
+foreach ($sequences as $sequence) {
+    $logger->log("Starting sequence: " . json_encode($sequence));
+
+    $lastSequence = $sequence;
+    $derivativeSequences = [];
+    do {
+        $derivative = getDiffs($lastSequence);
+        $logger->log("    " . json_encode($derivative));
+        $derivativeSequences[]= $derivative;
+        $lastSequence = $derivative;
+    } while(!isZeros($derivative));
+
+    $logger->log("    Found all zeros, working backwards to extrapolate");
+    $newValue = 0;
+    while ($derivative = array_pop($derivativeSequences)) {
+        $firstValue = $derivative[0];
+        $newValue = $firstValue - $newValue;
+        $logger->log("        Adding new value $newValue");
+    }
+
+    // Finally, we get back to our original sequence
+    $lastValue = $sequence[0];
+    $extrapolatedValue = $lastValue - $newValue;
+    $logger->log(    "Extrapolated $extrapolatedValue");
+    $extrapolated[]= $extrapolatedValue;
+}
+
+echo array_sum($extrapolated) . "\n";
